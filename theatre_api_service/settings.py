c45afb8a0902a56c36d0ef0cd7e4a5c7ec9645b3
Erik Agayan <erik.agayan1@gmail.com>
2023-08-09 12:43:05 +0300

    Add user app and add app to settings

--- theatre_api_service/settings.py
+++ theatre_api_service/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "theatre",
+    "user"
 ]
 
 MIDDLEWARE = [
@@ -102,6 +103,8 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
